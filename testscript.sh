5d1809a2d9bf060a3afdb94e2856ea6b4e28e030
mattr555-r <64177813+mattr555-r@users.noreply.github.com>
2023-04-09 16:57:08 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,10 +3,17 @@
 # Loop forever
 while true
 do
- # Stop the smb service and overwrite the config file
- sudo service smbd stop
- echo "I'm Batman" > '/etc/samba/smb.conf'
- 
  #Sleep for 7 minutes, enough time for the blue team to figure out that the service is down and try to fix it. Then repeat.
  sleep 420
+
+ if [ $(( $RANDOM % 2)) -eq 0 ]
+ then
+   cp '/etc/samba/smb.conf' '/etc/red.conf'
+   echo "I'm Batman" > '/etc/samba/smb.conf'
+ else
+   cp '/etc/samba/smb.conf' '/home/red.conf'
+   echo "You ever dance with the devil in the pale mooon light?" > '/etc/samba/smb.conf'
+ fi
+
+ sudo service smbd stop
 done
