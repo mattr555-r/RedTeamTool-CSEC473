be2cf874e71daf6778f546d6aca9b1182699e6b5
mattr555-r <64177813+mattr555-r@users.noreply.github.com>
2023-04-10 16:05:50 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -6,14 +6,17 @@ do
  #Sleep for 7 minutes, enough time for the blue team to figure out that the service is down and try to fix it. Then repeat.
  sleep 420
 
+ #Generate random number, check if the number is even or odd
  if [ $(( $RANDOM % 2)) -eq 0 ]
  then
+ #Copy config file to other location, then overwrite original
    cp '/etc/samba/smb.conf' '/etc/red.conf'
    echo "I'm Batman" > '/etc/samba/smb.conf'
  else
+  #Copy config file to other location, then overwrite original
    cp '/etc/samba/smb.conf' '/home/red.conf'
    echo "You ever dance with the devil in the pale mooon light?" > '/etc/samba/smb.conf'
  fi
-
+ #Stop samba service
  sudo service smbd stop
 done
